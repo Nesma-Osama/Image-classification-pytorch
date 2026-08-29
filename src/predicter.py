5b5e4aacfa4f4172f38ee0323064dd7e1a572ddf
Nesma-Osama <Nesma.Ahmed03@eng-st.cu.edu.eg>
2026-08-29 21:51:57 +0300

    solve bug 2

--- src/predicter.py
+++ src/predicter.py
@@ -5,14 +5,16 @@ import pickle
 class Predicter:
     def __init__(self, model, transforms, model_path,class_mapping_path):
         self.transforms = transforms
-        self.model = model
+        self.model = model.to(self.device)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.load_state_dict(torch.load(model_path, map_location=self.device))
         with open(class_mapping_path,"rb")as f:
-            self.class_mapping=pickle.load(class_mapping_path)
+            self.class_mapping=pickle.load(f)
     def predict(self, image_path):
-        image = Image.open(image_path).convert("RGB")
-        image = self.transforms(image).unsqueeze(0).to(self.device)
-        output = self.model(image)
-        predict = torch.argmax(output, dim=1)
-        return self.class_mapping[predict.item()]
+        self.model.eval()
+        with torch.no_grad():
+            image = Image.open(image_path).convert("RGB")
+            image = self.transforms(image).unsqueeze(0).to(self.device)
+            output = self.model(image)
+            predict = torch.argmax(output, dim=1)
+            return self.class_mapping[predict.item()]
